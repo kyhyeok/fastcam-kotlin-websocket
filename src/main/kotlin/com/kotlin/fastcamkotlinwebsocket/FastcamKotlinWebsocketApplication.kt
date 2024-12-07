f940ac212d0152a85ef74324d7e5ea8f2262b060
김윤혁 <kyhyeok@gmail.com>
2024-12-07 15:50:29 +0900

    Initial commit

---
+++ src/main/kotlin/com/kotlin/fastcamkotlinwebsocket/FastcamKotlinWebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.fastcamkotlinwebsocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcamKotlinWebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcamKotlinWebsocketApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/fastcamkotlinwebsocket/FastcamKotlinWebsocketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.fastcamkotlinwebsocket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcamKotlinWebsocketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
